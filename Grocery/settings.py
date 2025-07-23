97f468cc490a5ccd886d95a1123bb8e2a38f1476
Nitesh Bhardwaj <kumarnitesh52864@gmail.com>
2025-07-23 12:22:31 +0530

    updated

--- Grocery/settings.py
+++ Grocery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oqd*p@oabgte=4iwf8+*u&z#)ov0t%z(z!dx(o+)@#oq!zj+^@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'grocery-app-rjdg.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
